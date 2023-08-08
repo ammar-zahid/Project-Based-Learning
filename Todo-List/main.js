b16f9cda747cb6b517d3858dadf4798ce8219712
Ammar Zahid <131453458+ammar-zahid@users.noreply.github.com>
2023-08-08 21:41:22 +0500

    Working on todo list

---
+++ Todo-List/main.js
@@ -0,0 +1,30 @@
+const addTask = document.getElementById('add-task');
+const inputTask = document.getElementById('input-task');
+const taskContainer = document.getElementById('task-container');
+
+addTask.addEventListener('click', function(){
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class = "fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+    
+    let deleteButton = document.createElement('button');
+    checkButton.innerHTML = '<i class = "fa-solid fa-trash-can"></i>';
+    checkButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if(inputTask.value === ""){
+        alert('Please Enter a Task');
+    } else {
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = "";
+})
